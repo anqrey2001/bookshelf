280457ab8d5e9b9a094712afb22da81fe0268b11
anqrey2001 <66535819+anqrey2001@users.noreply.github.com>
2020-09-22 18:58:16 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,5 +7,34 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+	system ("chcp 1251"); 
+	int mode = -1;
+	vector<BookShelf> bselfs;
+	BookShelf bshl=elf;
+	while(mode!=0){
+		cout<<"-= Меню =- "<<endl;
+		cout<<"1. Добавить шкаф."<<endl;
+		cout<<"2. Добавить полку в шкаф"<<endl;
+		cout<<"3. Добавить книгу"<<endl;
+		cout<<"4. Вывод на экран"<<endl;
+		cout<<"0. Выход"<<endl;
+		cout<<"Введите номер и нажмите ENTER:";
+		cin>>mode;
+		switch(mode){
+			case 0: continue;
+			case 1:
+				bshelfs.push_back(bshelf);
+				cout<<"Шкаф добавлен!"<<endl<<endl;
+				break;
+				case 2
+				if(bshelfs.size()>0){
+				cout<<"Доступно "<<bshelfs.size()<<"шкафов"<<endl;
+				cout<<"Введите номер шкафа (от 1 до "shelfs.size()<<"): ";
+				int n = 0
+				cin>>n;
+				if(n>0 && n<=)
+				}
+		}
+	}
 	return 0;
 }
